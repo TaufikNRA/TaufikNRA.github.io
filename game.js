7af2ba909a517e8b8bde10ddb5712950e9121012
Taufik <taufiknurohmanafiko@gmail.com>
2025-03-25 11:15:21 +0700

    Update game.js

--- game.js
+++ game.js
@@ -9,9 +9,9 @@ function checkGuess() {
         document.getElementById('result').textContent = `Selamat! Kamu menebak dengan benar. Angka yang benar adalah ${secretNumber}.`;
         document.getElementById('attempts').textContent = `Percobaan: ${attempts}`;
     } else if (guess < secretNumber) {
-        document.getElementById('result').textContent = "Tebakanmu terlalu rendah! value: ${secretNumber}";
+        document.getElementById('result').textContent = `Tebakanmu terlalu rendah! value: ${secretNumber}.`;
     } else if (guess > secretNumber) {
-        document.getElementById('result').textContent = "Tebakanmu terlalu tinggi! value: ${secretNumber}";
+        document.getElementById('result').textContent = `Tebakanmu terlalu tinggi! value: ${secretNumber}.`;
     }
 
     document.getElementById('attempts').textContent = `Percobaan: ${attempts}`;
