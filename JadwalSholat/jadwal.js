7aaaee3ec071771f9dd12dce1882186b1f19dae8
Taufik_N_A <taufiknurohmanafiko@gmail.com>
2025-05-13 05:40:00 +0700

    Update jadwal.js

--- JadwalSholat/jadwal.js
+++ JadwalSholat/jadwal.js
@@ -9,31 +9,39 @@ function updateJam() {
 setInterval(updateJam, 1000);
 
 window.onload = function() {
+    document.getElementById("kota").style.display = "none";
+    document.getElementById("error-message").style.display = "none";
+    document.getElementById("tanggal").style.display = "none";
+    document.getElementById("jam-digital").style.display = "none";
+    
     function ambilNamaKota(lat, lon) {
-      fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`)
-        .then(res => res.json())
-        .then(data => {
-          const kota = data.address.city || data.address.town || data.address.village || data.address.county || "Lokasi Tidak Dikenal";
-          document.getElementById("kota").innerText = `${kota}`;
-        })
-        .catch(() => {
-          document.getElementById("kota").innerText = "Lokasi tidak ditemukan.";
-        });
+        fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`)
+            .then(res => res.json())
+            .then(data => {
+                const kota = data.address.city || data.address.town || data.address.village || data.address.county || "Lokasi Tidak Dikenal";
+                document.getElementById("kota").innerText = `${kota}`;
+            })
+            .catch(() => {
+                document.getElementById("error-message").innerText =
+                    "<span style='color:red'>Gagal memuat jadwal.</span>";"Lokasi tidak ditemukan.";
+                document.getElementById("error-message").style.display = "block";
+            });
     }
 
     if (navigator.geolocation) {
-       navigator.geolocation.getCurrentPosition(
-         pos => {
-           const lat = pos.coords.latitude;
-           const lon = pos.coords.longitude;
-           tampilkanJadwal(lat, lon);
-           ambilNamaKota(lat, lon);
-          },
-         () => {
-           document.getElementById("tanggal").innerHTML =
-             "<span style='color:red'>Izin lokasi ditolak. Jadwal tidak dapat ditampilkan.</span>";
-         }
-       );
+        navigator.geolocation.getCurrentPosition(
+            pos => {
+                const lat = pos.coords.latitude;
+                const lon = pos.coords.longitude;
+                tampilkanJadwal(lat, lon);
+                ambilNamaKota(lat, lon);
+            },
+            () => {
+                document.getElementById("error-message").innerHTML =
+                    "<span style='color:red'>Izin lokasi ditolak. Jadwal tidak dapat ditampilkan.</span>";
+                document.getElementById("error-message").style.display = "block";
+            }
+        );
     }
     updateJam();
 }
@@ -66,7 +74,9 @@ function tampilkanJadwal(lat, lon) {
             const [tgl, blnEn, thn] = readableDate.split(" ");
             const tanggalIndo = `${hariMap[weekdayEn]}, ${tgl} ${bulanMap[blnEn]} ${thn}`;
             document.getElementById("jadwal").style.transform = "translateX(-5%)";
+            document.getElementById("kota").style.display = "block";
             document.getElementById("tanggal").innerText = tanggalIndo;
+            document.getElementById("tanggal").style.display = "block";
             document.getElementById("jam-digital").style.display = "block";
             document.getElementById("sholat-times").innerHTML = `
                 <div><strong>${waktu.Fajr}</strong><br>Subuh</div>
@@ -76,12 +86,14 @@ function tampilkanJadwal(lat, lon) {
                 <div><strong>${waktu.Isha}</strong><br>Isya</div>
                 `;
         } else {
-            document.getElementById("tanggal").innerHTML =
-             "<span style='color:red'>Gagal memuat jadwal.</span>";
-         }
-      })
-       .catch(() => {
-        document.getElementById("tanggal").innerHTML =
-          "<span style='color:red'>Tidak dapat terhubung ke server.</span>";
-    });
+            document.getElementById("error-message").innerHTML =
+                "<span style='color:red'>Gagal memuat jadwal.</span>";
+            document.getElementById("error-message").style.display = "block";
+        }
+        })
+        .catch(() => {
+            document.getElementById("error-message").innerHTML =
+                "<span style='color:red'>Tidak dapat terhubung ke server.</span>";
+            document.getElementById("error-message").style.display = "block";
+        });
 }
