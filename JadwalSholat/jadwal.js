72ad4605cbe27c505f95275e3420f549c7991a4b
Taufik_N_A <taufiknurohmanafiko@gmail.com>
2025-05-13 04:02:49 +0700

    Create jadwal.js

---
+++ JadwalSholat/jadwal.js
@@ -0,0 +1,75 @@
+window.onload = function() {
+    function ambilNamaKota(lat, lon) {
+      fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`)
+        .then(res => res.json())
+        .then(data => {
+          const kota = data.address.city || data.address.town || data.address.village || data.address.county || "Lokasi Tidak Dikenal";
+          document.getElementById("kota").innerText = `${kota}`;
+        })
+        .catch(() => {
+          document.getElementById("kota").innerText = "Lokasi tidak ditemukan.";
+        });
+    }
+
+    if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(
+         pos => {
+           const lat = pos.coords.latitude;
+           const lon = pos.coords.longitude;
+           tampilkanJadwal(lat, lon);
+           ambilNamaKota(lat, lon);
+          },
+         () => {
+           document.getElementById("tanggal").innerHTML =
+             "<span style='color:red'>Izin lokasi ditolak. Jadwal tidak dapat ditampilkan.</span>";
+         }
+       );
+    }
+}
+
+function tampilkanJadwal(lat, lon) {
+    fetch(`https://api.aladhan.com/v1/timings?latitude=${lat}&longitude=${lon}&method=2`)
+        .then(res => res.json())
+        .then(data => {
+        if (data.code === 200) {
+            const waktu = data.data.timings;
+            const readableDate = data.data.date.readable;
+            const weekdayEn = data.data.date.gregorian.weekday.en;
+
+            const hariMap = {
+              "Monday": "Senin",
+              "Tuesday": "Selasa",
+              "Wednesday": "Rabu",
+              "Thursday": "Kamis",
+              "Friday": "Jumat",
+              "Saturday": "Sabtu",
+              "Sunday": "Minggu"
+            };
+
+            const bulanMap = {
+              "Jan": "Januari", "Feb": "Februari", "Mar": "Maret", "Apr": "April",
+              "May": "Mei", "Jun": "Juni", "Jul": "Juli", "Aug": "Agustus",
+              "Sep": "September", "Oct": "Oktober", "Nov": "November", "Dec": "Desember"
+            };
+
+            const [tgl, blnEn, thn] = readableDate.split(" ");
+            const tanggalIndo = `${hariMap[weekdayEn]}, ${tgl} ${bulanMap[blnEn]} ${thn}`;
+            document.getElementById("tanggal").innerText = tanggalIndo;
+            document.getElementById("jadwal").style.transform = "translateX(-5%)";
+            document.getElementById("sholat-times").innerHTML = `
+             <div><strong>${waktu.Fajr}</strong><br>Subuh</div>
+             <div><strong>${waktu.Dhuhr}</strong><br>Dzuhur</div>
+             <div><strong>${waktu.Asr}</strong><br>Ashar</div>
+             <div><strong>${waktu.Maghrib}</strong><br>Maghrib</div>
+             <div><strong>${waktu.Isha}</strong><br>Isya</div>
+             `;
+        } else {
+            document.getElementById("tanggal").innerHTML =
+             "<span style='color:red'>Gagal memuat jadwal.</span>";
+         }
+      })
+       .catch(() => {
+        document.getElementById("tanggal").innerHTML =
+          "<span style='color:red'>Tidak dapat terhubung ke server.</span>";
+    });
+}
